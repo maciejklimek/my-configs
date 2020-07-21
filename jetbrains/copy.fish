aa156f0a9ae19da29597d1108fbfab45c7cb1929
Maciej Klimek <maciej.klimek@gmail.com>
2020-07-21 12:09:07 +0200

    GNOME maciek.xml

---
+++ jetbrains/copy.fish
@@ -0,0 +1,2 @@
+ln -s  (pwd)/GNOME\ maciek.xml /home/maciek/.AndroidStudio4.0/config/keymaps/GNOME\ maciek.xml
+ln -s  (pwd)/GNOME\ maciek.xml /home/maciek/.config/JetBrains/PyCharm2020.1/keymaps/GNOME\ maciek.xml
